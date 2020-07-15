1fb0fd71014c27bb91671fe68125dcaf94f5b785
TheMC42 <57400099+TheMC42@users.noreply.github.com>
2020-07-15 09:54:10 -0500

    Commit Principal

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+
+    private void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public float spawnDelay = 0.5f;
+
+    public IEnumerator RespawnPlayer()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds (spawnDelay);
+
+        Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        
+    }
+
+    public static void KillPlayer( Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+        
+    }
+
+}
